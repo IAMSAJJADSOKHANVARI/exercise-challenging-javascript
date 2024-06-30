ef96e337559eace6fba00c4948851f0a4f85adeb
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2024-06-30 10:15:20 +0330

    i finished foutth challenging

---
+++ challenge/challenge-5/script.js
@@ -0,0 +1,51 @@
+Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+Hints:
+§ To calculate average of 3 values, add them all together and divide by 3
+§ To check if number A is at least double number B, check for A >= 2 * B.
+Apply this to the team's average scores
+
+
+
+**********************************************
+const calcAverage =(a,b,c) =>(a+b+c)/3 ;
+console.log(calcAverage (3,4,5))
+//test 1 
+let scoreDolphins = calcAverage(44,23,71);
+let scoreKoalas = calcAverage(65,54,49);  
+console.log(` D:${scoreDolphins},K:${scoreKoalas}`);
+
+const checkWinner=function(avgDolhins ,avgKoalas){
+  if(avgDolhins >=2 * avgKoalas){
+    console.log(`dolphins win 👑(${avgDolhins} vs
+      . ${avgKoalas})`);
+
+  }else if (avgKoalas >= 2 * avgDolhins){
+    console.log(`Koalas win 👑(${avgKoalas} vs.
+       ${avgDolhins})`);
+
+  }else{
+    console.log('No team wins...');
+  }
+}
+checkWinner(scoreDolphins , scoreKoalas);
+
+checkWinner(576 , 111);
+
+//test2 
+scoreDolphins = calcAverage(85,54,41);
+scoreKoalas=calcAverage(23,34,27);
+console.log (scoreDolphins,scoreKoalas);
+checkWinner(scoreDolphins,scoreKoalas)
+
+
